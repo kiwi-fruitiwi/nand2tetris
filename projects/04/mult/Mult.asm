7526fd4461cad81df6b027e971e39a8fe95e7019
kiwi-fruitiwi <zane.tian@gmail.com>
2021-12-31 06:52:26 -0800

    add mult.asm

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -10,3 +10,52 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+
+// start by assigning variables:
+// n=R0
+// i=0
+// sum=0
+@R0
+D=M
+@n
+M=D
+@i
+M=0
+@sum
+M=0
+
+// this is our loop
+(LOOP)
+@i
+D=M 		// assign i to D
+@R1
+D=D-M 		// D = i-R1
+@STOP
+D;JGE 		// if i>=R1, goto STOP. JGE checks if i-R1>=0, or i>=R1
+
+// calculate sum += i
+@sum
+D=M
+@n
+D=M+D 		// sum + n
+@sum
+M=D 		// sum += n
+
+// i++
+@i
+M=M+1
+
+// loop
+@LOOP
+0;JMP
+
+(STOP)
+@sum
+D=M
+@R2
+M=D
+
+(END)
+@END
+0,JMP
